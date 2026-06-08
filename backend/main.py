4ff5e205ac447d8f0cceb4dc408b9d158442b0c8
Chirag Jindani <chiragjindani2006@example.com>
2026-06-08 14:02:47 +0530

    .vercel.app

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://georef-tool.app"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://georef-tool.vercel.app/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
